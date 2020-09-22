19a032dcb71c0dccfad96c4cf0536501b7f4f4bb
JennStro <jennys1994@gmail.com>
2020-09-22 20:15:40 +0200

    Add updating

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,57 @@
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import static spark.Spark.*;
 
 public class Main {
+
+    private static List<Todo> items = new ArrayList<>();
+
+    public static Todo fromJson(String json) {
+        return new Gson().fromJson(json, Todo.class);
+    }
 
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World!");
 
         Todo todoItem = new Todo();
+        todoItem.setId(UUID.randomUUID());
         todoItem.setDescription("Empty trash can");
         todoItem.setSummary("Summary");
+        items.add(todoItem);
 
         String jsonTodo = new Gson().toJson(todoItem);
 
         get("/todo", (req, res) -> jsonTodo);
+
+        post("/", (req, res) -> {
+            Todo item = fromJson(req.body());
+            item.setId(UUID.randomUUID());
+            items.add(item);
+            return new Gson().toJson(item);
+        });
+
+        put("/", (request, response) -> {
+            Todo updatedItem = new Gson().fromJson(request.body(), Todo.class);
+            System.out.println(request.body());
+            System.out.println(items);
+            for (int i = 0; i < items.size(); i++) {
+                if (items.get(i).getId().equals(updatedItem.getId())) {
+                    items.set(i, updatedItem);
+                    return updatedItem.toJson();
+                }
+            }
+            return "Could not update";
+        });
+
+        delete("/", (request, response) -> {
+            return request.body();
+        });
+
+
     }
 
 }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,6 +1,9 @@
+import com.google.gson.Gson;
+import java.util.UUID;
 
 public class Todo {
 
+    private UUID id;
     private String summary;
     private String description;
 
@@ -20,11 +23,24 @@ public class Todo {
         this.description = description;
     }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "Todo{" +
-                "summary='" + summary + '\'' +
+                "id=" + id +
+                ", summary='" + summary + '\'' +
                 ", description='" + description + '\'' +
                 '}';
+    }
+
+    public String toJson() {
+        return new Gson().toJson(this);
     }
 }
