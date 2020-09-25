6a3b9f5c20b495efbe6954ab354babb6712e339e
JennStro <jennys1994@gmail.com>
2020-09-25 13:54:13 +0200

    Remove unused code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,18 @@
 import com.google.gson.Gson;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import static spark.Spark.*;
 
 public class Main {
 
-    private static List<Todo> items = new ArrayList<>();
-
     private static TodoRepository repository = new TodoRepository();
     private static TodoService service = new TodoService(repository);
-
 
     public static Todo fromJson(String json) {
         return new Gson().fromJson(json, Todo.class);
     }
 
     public static void main(String[] args) {
-
-        Todo todoItem = new Todo();
-        todoItem.setId(UUID.randomUUID());
-        todoItem.setDescription("Empty trash can");
-        todoItem.setSummary("Summary");
-        items.add(todoItem);
 
         get("/allTodos", (req, res) ->  service.getAllTodos());
 
